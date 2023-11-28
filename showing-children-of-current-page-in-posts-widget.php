97cd0608ada1088ecc83f3061f6fd6e5f3e12c59
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2023-11-28 10:59:16 +0300

    Create showing-children-of-current-page-in-posts-widget.php

---
+++ showing-children-of-current-page-in-posts-widget.php
@@ -0,0 +1,9 @@
+<?php
+add_action( 'elementor/query/children_filter', function( $query ) {
+	$current_pageID = get_queried_object_id();
+	// Modify the query
+	$query->set( 'post_parent', $current_pageID );
+});
+
+// Disable elementor google fonts
+add_filter( 'elementor/frontend/print_google_fonts', '__return_false' );
